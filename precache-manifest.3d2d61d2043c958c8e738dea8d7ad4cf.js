4f693e27c8a402ad430b6c9611877d171d9a7ba1
Code Sandbox <hello@codesandbox.io>
2022-08-13 01:51:11 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.3d2d61d2043c958c8e738dea8d7ad4cf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "65ddac619b593d53e2dd",
+    "url": "https://oftenalways.github.io/csb-mtz2rt/static/js/main.65ddac61.chunk.js"
+  },
+  {
+    "revision": "b886e618b2357c8cd4ce",
+    "url": "https://oftenalways.github.io/csb-mtz2rt/static/js/1.b886e618.chunk.js"
+  },
+  {
+    "revision": "a34e5ea7b594bf778e08",
+    "url": "https://oftenalways.github.io/csb-mtz2rt/static/js/runtime~main.a34e5ea7.js"
+  },
+  {
+    "revision": "2eacd52225addbe96acc087dc0764ceb",
+    "url": "https://oftenalways.github.io/csb-mtz2rt/index.html"
+  }
+];
